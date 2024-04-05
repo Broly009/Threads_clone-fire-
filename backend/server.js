2c70d7cbe60aff843269d5a93acecb5f4ec831a1
Antu Golder <antugolder685@gmail.com>
2024-04-05 18:37:02 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -29,11 +29,11 @@ app.use("/api/users",userRoutes)
 app.use("/api/posts",postRoutes)
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/thread/dist")));
 
 	// react app
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "thread", "dist", "index.html"));
 	});
 }
 
